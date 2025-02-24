4bc2d8155f3ecbeef123c1e23a91e9f512381f79
Raksha <rak070803@gmail.com>
2025-02-24 08:26:33 +0530

    [Raksha] add. Creating employee payroll Spring Project to cater to REST request form employee payroll UI

---
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/EmployeepayrollappdevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeepayrollappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrollappdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrollappdevelopmentApplication.class, args);
+
+	}
+
+}
---
+++ employeepayrollappdevelopment/src/test/java/com/example/employeepayrollappdevelopment/EmployeepayrollappdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeepayrollappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepayrollappdevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
