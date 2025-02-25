33553f04a8ce6fac7133cdc768c727adb54ae6af
Raksha <rak070803@gmail.com>
2025-02-25 15:52:49 +0530

    [Raksha] Add.EmployeePayrollApp Use Lombok to Auto-generate Getters & Setters for DTO

--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
@@ -2,8 +2,9 @@ package com.example.employeepayrollappdevelopment.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data; //lombok used to auto generate getter and setter methods.
 
-
+@Data
 public class EmployeePayrollDTO {
     @NotEmpty(message = "Name cannot be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z\\s]+$", message = "Name should start with uppercase")
