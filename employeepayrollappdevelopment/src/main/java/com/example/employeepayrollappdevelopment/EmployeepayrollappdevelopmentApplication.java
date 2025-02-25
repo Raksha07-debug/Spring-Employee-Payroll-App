90d61b0e0a410e42d69e26d103f2a4f1da5e94a3
Raksha <rak070803@gmail.com>
2025-02-25 16:21:41 +0530

    [Raksha] Add Configure logging for different environment dev production

--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/EmployeepayrollappdevelopmentApplication.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/EmployeepayrollappdevelopmentApplication.java
@@ -1,14 +1,18 @@
 package com.example.employeepayrollappdevelopment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeepayrollappdevelopmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeepayrollappdevelopmentApplication.class, args);
-
+		ApplicationContext context= SpringApplication.run(EmployeepayrollappdevelopmentApplication.class,args);
+		log.info("Payroll App  started in '{}' Environment !",context.getEnvironment().getProperty("environment"));
 	}
 
 }
