c7c7926ec6c1a953543b94ae2ee0a79b75b3a001
Raksha <rak070803@gmail.com>
2025-02-27 11:01:40 +0530

    [Raksha] Add.EmployeePayrollApp Ability to throw User Friendly Errors in case Employee Id is not found

--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
@@ -8,7 +8,8 @@ import lombok.Data; //lombok used to auto generate getter and setter methods.
 @Data
 public class EmployeePayrollDTO {
     @NotEmpty(message = "Name cannot be empty")
-    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")    public String name;
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{1,}(?: [A-Z][a-zA-Z]{1,})+$", message = "Name must contain both first name and last name, each starting with a capital letter and be at least 2 characters long")
+    public String name;
 
     @Min(value = 1000, message = "Salary must be at least 1000")
     @Max(value = 1000000, message = "Salary cannot exceed 1,000,000")
--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/exception/GlobalExceptionHandler.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.employeepayrollappdevelopment.exception;
 
+import com.example.employeepayrollappdevelopment.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,14 +15,20 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>>
-    handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        ResponseDto responseDTO = new ResponseDto("Validation Error", errors);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
-}
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<ResponseDto> handleEmployeePayrollException(EmployeeNotFoundException exception){
+        ResponseDto responseDto=new ResponseDto("exception while processing REST request",exception.getMessage());
+        return new ResponseEntity<>(responseDto,HttpStatus.BAD_REQUEST);
+    }
+    }
+
