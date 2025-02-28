8b5c5989f09d01d7e1e41961eba931f36185eeac
Raksha <rak070803@gmail.com>
2025-02-28 14:47:15 +0530

    [Raksha] Add. Add properties like startDate note profilePic and department

--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/dto/EmployeePayrollDTO.java
@@ -4,8 +4,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data; //lombok used to auto generate getter and setter methods.
+import lombok.ToString;
 
-@Data
+import java.time.LocalDate;
+import java.util.List;
+
+@ToString
 public class EmployeePayrollDTO {
     @NotEmpty(message = "Name cannot be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z]{1,}(?: [A-Z][a-zA-Z]{1,})+$", message = "Name must contain both first name and last name, each starting with a capital letter and be at least 2 characters long")
@@ -13,15 +17,12 @@ public class EmployeePayrollDTO {
 
     @Min(value = 1000, message = "Salary must be at least 1000")
     @Max(value = 1000000, message = "Salary cannot exceed 1,000,000")
-    private long salary;
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO{name='" + name + "', salary=" + salary + "}";
-    }
+    public int salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 
 
 }
--- employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/model/Employee.java
+++ employeepayrollappdevelopment/src/main/java/com/example/employeepayrollappdevelopment/model/Employee.java
@@ -1,7 +1,11 @@
 package com.example.employeepayrollappdevelopment.model;
 
+import com.example.employeepayrollappdevelopment.dto.EmployeePayrollDTO;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name="employees")
@@ -11,8 +15,24 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
-    private String role;
-    private String department;
+    private String gender;
     private int salary;
+    private String note;
+    private LocalDate startDate;
+    private String profilePic;
+    private List<String> department;
+
+    public Employee(int id, EmployeePayrollDTO employeePayroll) {
+        this.id=id;
+        this.name = employeePayroll.name;
+        this.salary = employeePayroll.salary;
+        this.gender=employeePayroll.gender;
+        this.startDate=employeePayroll.startDate;
+        this.note=employeePayroll.note;
+        this.profilePic=employeePayroll.profilePic;
+        this.department=employeePayroll.department;
+    }
+
 }
